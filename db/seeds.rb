25a550ae66b67f69a739b67f3c8691790b2dc9bf
Eric Williamson <ericwilli@gmail.com>
2022-08-16 17:58:44 +0800

    created fucntional app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Article.destroy_all
+
+10.times do
+  title = Faker::Book.title
+  content = Faker::Quotes::Shakespeare.hamlet_quote
+  Article.create(title: title, content: content)
+  puts "title: #{title}, content: #{content}"
+end
+
+puts "Total articles: #{Article.count}"
